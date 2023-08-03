75f4d0156af5f45d00679ef150419f53a60486d0
Dave Welsh <dave@qccareerschool.com>
2023-08-03 10:11:03 -0400

    Fixed dev mode ip address

--- src/middleware/ipAddress.ts
+++ src/middleware/ipAddress.ts
@@ -5,17 +5,18 @@ dotenv.config();
 
 export const ipAddressMiddleware: RequestHandler = (req, res, next) => {
   let ipAddress: string | null = null;
-  const forwardedFor = req.headers['x-forwarded-for'];
-  if (Array.isArray(forwardedFor) && forwardedFor.length) {
-    ipAddress = forwardedFor[0].split(',')[0].trim();
-  } else if (typeof forwardedFor === 'string') {
-    ipAddress = forwardedFor.split(',')[0].trim();
-  } else if (req.socket.remoteAddress) {
-    ipAddress = req.socket.remoteAddress;
-  }
-  if (process.env.MODE === 'test') {
+  if (process.env.MODE !== 'production') {
     ipAddress = '135.23.119.183';
+  } else {
+    const forwardedFor = req.headers['x-forwarded-for'];
+    if (Array.isArray(forwardedFor) && forwardedFor.length) {
+      ipAddress = forwardedFor[0].split(',')[0].trim();
+    } else if (typeof forwardedFor === 'string') {
+      ipAddress = forwardedFor.split(',')[0].trim();
+    } else if (req.socket.remoteAddress) {
+      ipAddress = req.socket.remoteAddress;
+    }
   }
-  res.locals.location = ipAddress;
+  res.locals.ipAddress = ipAddress;
   next();
 };
