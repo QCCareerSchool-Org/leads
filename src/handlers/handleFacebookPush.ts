7775307bc516605ad82e82bca61cacf436eb8323
Dave Welsh <dave@qccareerschool.com>
2026-01-05 12:57:53 -0500

    Updated dependencies

--- src/handlers/handleFacebookPush.ts
+++ src/handlers/handleFacebookPush.ts
@@ -1,36 +1,96 @@
 import type { Request, Response } from 'express';
 import z from 'zod';
 
-import { isSchoolName } from '../domain/school.js';
-import { faceBookAddPush } from '../interactors/facebookPush.js';
+import { isSchoolSlug } from '../domain/school.js';
+import { facebookAddPush, facebookFetchData } from '../interactors/facebookPush.js';
+import type { ResultType } from '../lib/result.js';
+import { Result } from '../lib/result.js';
 
-const schema = z.object({
-  firstName: z.string(),
-  lastName: z.string(),
-});
+export interface FBChange {
+  field: string;
+  value: {
+    verb: string;
+    /** numerical string */
+    object_id: string;
+  };
+}
 
-export const handleFacebookPush = async (req: Request<{ school: string }>, res: Response) => {
-  const school = req.params.school;
-  if (!isSchoolName(school)) {
+export interface FBEntry {
+  time: number;
+  changes: FBChange[];
+  /** numerical string */
+  id: string;
+  /** numerical string */
+  uid: string;
+};
+
+export interface FBRequest {
+  entry: FBEntry[];
+  object: string;
+}
+
+export const handleFacebookPush = async (req: Request<{ schoolSlug: string }>, res: Response) => {
+  const schoolSlug = req.params.schoolSlug;
+  if (!isSchoolSlug(schoolSlug)) {
     res.status(400).send('Invalid school');
     return;
   }
 
-  const input = await schema.safeParseAsync(req.body);
-  if (input.error) {
-    res.status(400).send(input.error.message);
+  const validateResult = await validateRequest(req.body);
+  if (!validateResult.success) {
+    res.status(400).send(validateResult.error.message);
     return;
   }
+  const body = validateResult.value;
 
-  const result = await faceBookAddPush(input.data.firstName, input.data.lastName);
+  for (const entry of body.entry) {
 
-  if (result.success) {
-    res.status(200).send(result.value);
-  } else {
-    const error = result.error;
-    switch (error.constructor) {
-      default:
-        res.status(500).send(result.error.message);
+    for (const change of entry.changes) {
+
+      if (change.field !== '') {
+        continue;
+      }
+
+      const fetchResult = await facebookFetchData(change);
+      if (!fetchResult.success) {
+        res.status(500).send(fetchResult.error.message);
+        return;
+      }
+
+      const addResult = await facebookAddPush(change);
+      if (!addResult.success) {
+        switch (addResult.error.constructor) {
+          default:
+            res.status(500).send(addResult.error.message);
+        }
+        return;
+      }
     }
   }
+
+  res.status(200).end();
+};
+
+const schema = z.object({
+  entry: z.array(z.object({
+    time: z.number(),
+    changes: z.array(z.object({
+      field: z.string(),
+      value: z.object({
+        verb: z.string(),
+        object_id: z.string(), // eslint-disable-line camelcase
+      }),
+    })),
+    id: z.string(),
+    uid: z.string(),
+  })),
+  object: z.string(),
+});
+
+const validateRequest = async (requestBody: Request['body']): Promise<ResultType<FBRequest>> => {
+  const result = await schema.safeParseAsync(await requestBody);
+  if (result.error) {
+    return Result.fail(Error(result.error.message));
+  }
+  return Result.success(result.data);
 };
--- src/interactors/facebookPush.ts
+++ src/interactors/facebookPush.ts
@@ -1,6 +1,11 @@
+import type { FBChange } from '../handlers/handleFacebookPush.js';
 import type { ResultType } from '../lib/result.js';
 import { Result } from '../lib/result.js';
 
-export const faceBookAddPush = async (firstName: string, lastName: string): Promise<ResultType<string>> => {
-  return Result.success(`${firstName} ${lastName}`);
+export const facebookAddPush = async (entry: FBChange): Promise<ResultType<string>> => {
+  return Result.success(``);
+};
+
+export const facebookFetchData = async (entry: FBChange): Promise<ResultType<number>> => {
+  return Result.success(0);
 };
